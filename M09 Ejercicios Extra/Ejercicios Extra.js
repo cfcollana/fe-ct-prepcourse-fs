2bf2360a24533439cc41e3ee7c0c5de32ffa0ad4
cfcollana <cfcollana@gmail.com>
2023-12-11 19:27:04 -0500

    Ejercicios M09

--- M09 Ejercicios Extra/Ejercicios Extra.js
+++ M09 Ejercicios Extra/Ejercicios Extra.js
@@ -6,6 +6,12 @@ function deObjetoAarray(objeto) {
    // Estos elementos debe ser cada par clave:valor del objeto recibido.
    // [EJEMPLO]: {D: 1, B: 2, C: 3} ---> [['D', 1], ['B', 2], ['C', 3]].
    // Tu código:
+   var arrayNuevo = [];
+
+   for(var clave in objeto){
+      arrayNuevo.push([clave,objeto[clave]]);
+   }
+   return arrayNuevo;
 }
 
 function numberOfCharacters(string) {
@@ -14,6 +20,16 @@ function numberOfCharacters(string) {
    // Las letras deben estar en orden alfabético.
    // [EJEMPLO]: "adsjfdsfsfjsdjfhacabcsbajda" ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 }
    // Tu código:
+  
+   var objLetra = {};
+   for (var i = 0; i < string.length; i++){
+       if(objLetra.hasOwnProperty(string[i])){
+          objLetra[string[i]] = objLetra[string[i]] + 1;    
+       } else {
+         objLetra[string[i]] = 1;    
+       }
+   }
+   return objLetra;
 }
 
 function capToFront(string) {
@@ -22,6 +38,17 @@ function capToFront(string) {
    // Retornar el string.
    // [EJEMPLO]: soyHENRY ---> HENRYsoy
    // Tu código:
+   var stringMay = '';
+   var stringMin = '';
+
+   for (var i = 0; i < string.length; i++){
+       if(string[i] === string[i].toLowerCase()){
+         stringMin = stringMin + string[i];
+       }else {
+         stringMay = stringMay + string[i];
+       }
+   }
+   return stringMay + stringMin;
 }
 
 function asAmirror(frase) {
@@ -29,18 +56,50 @@ function asAmirror(frase) {
    // La diferencia es que cada palabra estará escrita al inverso.
    // [EJEMPLO]: "The Henry Challenge is close!"  ---> "ehT yrneH egnellahC si !esolc"
    // Tu código:
+   //   var palabraInverso = '';
+   //   var array = [];
+   //   array = frase.split(' ');
+   //   array.forEach (function(val){
+   //     for(var i = val.length-1; i >= 0; i-- ){
+   //         palabraInverso = palabraInverso + val[i] ;  
+   //      }
+   //         palabraInverso = palabraInverso + ' ' ;
+   //   })
+   //  return palabraInverso;
+   var array = frase.split(' ');
+   var arrayFinal = array.map(function(ele){
+       return ele.split('').reverse().join('');
+   })
+       return arrayFinal.join(' ');
 }
 
 function capicua(numero) {
    // Si el número que recibes es capicúa debes retornar el string: "Es capicua".
    // Caso contrario: "No es capicua".
    // Tu código:
+   var string = numero.toString();
+   var capicua = string.split('').reverse().join('');
+   if (string === capicua){
+       return 'Es capicua';
+   } else{
+       return 'No es capicua';
+
+}
 }
 
 function deleteAbc(string) {
    // Tu tarea es eliminar las letras "a", "b" y "c" del string recibido.
    // Retorna el string sin estas letras.
    // Tu código:
+   var stringSinABC = '';
+   for (var i=0; i < string.length; i++){
+      if(string[i] === 'a' || string[i] === 'b' || string[i] === 'c'){
+         continue;
+      }else{
+         stringSinABC = stringSinABC + string[i];
+      }
+   } 
+   return stringSinABC;
 }
 
 function sortArray(arrayOfStrings) {
@@ -49,6 +108,19 @@ function sortArray(arrayOfStrings) {
    // de la longitud de cada string.
    // [EJEMPLO]: ["You", "are", "beautiful", "looking"]  ---> [“You", "are", "looking", "beautiful"]
    // Tu código:
+   var cambio = true;
+   while(cambio){
+      cambio = false;
+      for(var i=0; i< arrayOfStrings.length - 1; i++){
+         if(arrayOfStrings[i].length > arrayOfStrings[i+1].length){ 
+         var aux = arrayOfStrings[i];
+         arrayOfStrings[i] = arrayOfStrings[i+1];
+         arrayOfStrings[i+1] = aux;
+         cambio = true
+         }
+      }
+   }
+   return arrayOfStrings;
 }
 
 function buscoInterseccion(array1, array2) {
@@ -58,6 +130,16 @@ function buscoInterseccion(array1, array2) {
    // Si no tienen elementos en común, retornar un arreglo vacío.
    // [PISTA]: los arreglos no necesariamente tienen la misma longitud.
    // Tu código:
+
+   var arrayInterseccion = [];
+   for( var i = 0; i < array1.length; i++){
+      for (var j = 0; j < array2.length; j++){
+         if (array1[i] === array2[j]){
+            arrayInterseccion.push(array1[i]);
+         }
+      }
+   }
+   return arrayInterseccion;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
--- preCourse.js
+++ preCourse.js
@@ -1,91 +1,16 @@
-// tipo de dato string
-var nombre = 'Christian Collana';
+var numero = 525;
+var frase = 'The Henry Challenge is close!'
+var string = numero.toString();
+var resultado = frase.split(' ').reverse().join(' ');
+//var resultado = frase.split(' ');
+//resultado.reverse();
+//resultado.join('');
+console.log (resultado);
 
-// tipo de dato number
-var edad = 46;
-var edad2 = '46';
+//if(string === resultado){
+//    console.log ('Es capicua')
+//} else{
+//    console.log ('No es capicua')
+//}
 
-console.log('comparacion ==', edad == edad2)
-console.log('comparacion ===', edad === edad2)
-
-// tipo de dato boolean
-var esEstudiante = true;
-var esProfesor = false;
-
-function esEstudianteFunc(condicion) {
-    if (esEstudiante === condicion) {
-        console.log ('ES ESTUDIANTE')
-        return true
-    } else {
-        console.log ('NO ES ESTUDIANTE')
-        return false
-    }
-}
-
-console.log(esEstudianteFunc(false));
-
-
-// tipo de dato undefined
-var x;
-
-// funciones
-function saludar(nombre){
-    return "hola " + nombre;
-}
-
-
-console.log(saludar('Christian'))
-
-// Operadores
-
-console.log(1+1);
-console.log(10/2);
-console.log(5*4);
-console.log(10 ** 2);
-console.log(49%2);
-
-// operdaores de comparacion
-
-console.log (1==1);
-console.log (1 !=2);
-console.log (1==='1');
-console.log (4>=4);
-console.log (4>=3);
-
-// operadores logicos
-
-console.log (1 == 1) && (2 == 2);
-console.log (1 == 1) && (2 == 3);
-
-var nombre = 'Chritsian';
-console.log(nombre.length)
-
-function idioma(key) {
-    if (key === 'es') {
-        return 'HOLA'
-    } else if (key === 'en') {
-        return 'HELLO'
-    } else {
-        return 'No es un idioma valido'
-    }
-}
-
-console.log(idioma('fr'));
-
-console.log(Math.pow(2,5));
-
-var numeroRandom = Math.random();
-console.log(numeroRandom * 10);
-console.log(Math.ceil(numeroRandom * 10));
-
-// es par
-function esPar (numero) {
-    if (numero % 2 === 0) {
-        return 'Es par'
-    } else {
-        return 'Es impar'
-    }
-}
-console.log(esPar(25));
-
-
+ 
